96019e89a320fa441693dddf719d54721698191e
priambodop <priambodop>
2017-09-13 21:24:47 +0700

    create nodejs file

---
+++ NodeJs/myfirst.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function(req, res){
+  fs.readFile('demofile1.html',function(err, data){
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.write(data);
+    res.end();
+  });
+
+  fs.writeFile('mynewfile.txt','this is my text',function(err){
+    if (err) throw err;
+    console.log('Replaced!');
+  });
+}).listen(3000);
